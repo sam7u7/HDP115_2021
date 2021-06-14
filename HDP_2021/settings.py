c1b4e0feb1f3c89b8a6b03106a8f4a88599106ff
unknown <ba18048@ues.edu.sv>
2021-06-14 02:21:16 -0600

    update migraciones y estructura de archivo

--- HDP_2021/settings.py
+++ HDP_2021/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Principal',
+    'Principal.Usuarios',
+    'Principal.Asignaciones',
+    'Principal.PaquetesAlimentarios',
 ]
 
 MIDDLEWARE = [
