a62556dd3caa4bd229f728ade4e5f277b85c6d7b
unknown <ba18048@ues.edu.sv>
2021-06-15 22:18:47 -0600

    update

--- HDP_2021/urls.py
+++ HDP_2021/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from asignaciones.views import asignacionIndex
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('asignasion/index',asignacionIndex,name='index')
 ]
